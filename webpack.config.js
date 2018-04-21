6d9ccd9423973845cfbdee77aeade5fb800f7c2f
tengattack <tengattack@foxmail.com>
2018-04-21 17:30:57 +0900

    Update webpack devtool config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const path = require('path')
 const publicPath = path.join(__dirname, 'public')
 const config = {
   entry: './src/js/app.js',
+  devtool: process.env.NODE_ENV === 'production' ? false : 'eval',
   output: {
     filename: 'bundle.js',
     publicPath: 'js/assets',
